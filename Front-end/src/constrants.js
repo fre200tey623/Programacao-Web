8bb9aaa9c530a9f8a402a39aac9a9499a8f73143
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-06-24 16:34:27 -0300

    Novas constantes adicionadas

---
+++ Front-end/src/constrants.js
@@ -0,0 +1,69 @@
+export const info = [
+    {
+        title: "Dataset estatistica declaração de importação para o Brasil"
+    }
+]
+
+
+export const question = [
+    {
+        id:"1",
+        name: "Qual o mês e a quantidade de vezes que cada país declarou importação para o Brasil?",
+        description: [
+            "Essa é a 1 descricao do 1 botao",
+            "Essa é a 2 descricao do 1 botao",
+            "Essa é a 3 descricao do 1 botao"
+        ]
+    },
+    {
+        id:"2",
+        name: "Qual o maior valor máximo de frete dólar truncado da Argentina e seu respectivo mês?",
+        description: ["Essa é a descricao do 2 botao"]
+    },
+    {
+        id:"3",
+        name: "Qual os dez países que mais declaram importação para o Brasil, nos meses de Janeiro a Abril de 2023",
+        description: ["Essa é a descricao do 3 botao"]
+    },
+    {
+        id:"4",
+        name: "Qual o número total de importações de cada país para o Brasil, nos meses de Janeiro a Abril de 2023?",
+        description: ["Essa é a descricao do 4 botao"]
+    },
+    {
+        id:"5",
+        name: "Qual a quantidade de declarações de importação de forma crescente de cada mês para o Brasil?",
+        description: ["Essa é a descricao do 5 botao"]
+    },
+    {
+        id:"6",
+        name: "Qual país que mais paga por importação para o Brasil em todos os meses?",
+        description: ["Essa é a descricao do 6 botao"]
+    },
+    {
+        id:"7",
+        name: "Pegando os dados da China (República Popular), dentre os meses de janeiro a abril, qual foi o mês que teve menos importações?",
+        description: ["Essa é a descricao do 7 botao"]
+    },
+    {
+        id:"8",
+        name: "Qual a média dos fretes do mês de declarações de importações entre os países?",
+        description: ["Essa é a descricao do 8 botao"]
+    },
+    {
+        id:"9",
+        name: "Qual foi os 5 países que tiveram as cargas mais pesadas?",
+        description: ["Essa é a descricao do 9 botao"]
+    },
+    {
+        id:"10",
+        name: "Quais os 5 países em média pagam maior seguro, segundo a declaração de importação para o Brasil?",
+        description: ["Essa é a descricao do 10 botao"]
+    }
+
+
+
+];
+
+
+
