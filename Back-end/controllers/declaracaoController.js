189793b7c3fbb2d65c4aa51558905a4b34c5d24c
lulinha <luisfernandosouzadearaujo22@gmail.com>
2023-06-24 19:40:56 -0300

    .

--- Back-end/controllers/declaracaoController.js
+++ Back-end/controllers/declaracaoController.js
@@ -14,7 +14,10 @@ exports.getQuestao1 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao2 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $match: { pais_origem: "ARGENTINA" } }, { $sort: { frete_maximo_dolar_truncado: -1 } }, { $project: { pais_origem: "$pais_origem", frete_maximo_dolar_truncado:"$frete_maximo_dolar_truncado", mes: "$mes" } }, { $limit: 1 } ]
+    const declaracoes = await Declaracao.aggregate( [ { $match: { pais_origem: "ARGENTINA" } }, 
+    { $sort: { frete_maximo_dolar_truncado: -1 } }, 
+    { $project: {_id:0, pais_origem: "$pais_origem", frete_maximo_dolar_truncado:"$frete_maximo_dolar_truncado", mes: "$mes" } }, 
+    { $limit: 1 } ]
     );
     res.status(200).json({
         status: 'success',
@@ -58,7 +61,7 @@ exports.getQuestao5 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao6 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $group: { _id: "$pais_origem", media: { $avg : "$seguro_media_dolar_truncado" } } }, { $project: { valor_seguro: "$media" } }, { $sort: { valor_seguro: -1 } }, { $limit: 1 } ]
+    const declaracoes = await Declaracao.aggregate([ { $group: { _id: "$pais_origem", media: { $avg : "$seguro_media_dolar_truncado" } } }, { $project: {_id:0, pais_origem:"$_id", valor_seguro: "$media" } }, { $sort: { valor_seguro: -1 } }, { $limit: 1 } ]
     );
     res.status(200).json({
         status: 'success',
@@ -69,7 +72,23 @@ exports.getQuestao6 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao7 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $match: { pais_origem: "CHINA, REPUBLICA POPULAR" } }, { $group: { _id: "$mes", count: { $sum: 1 } } }, { $sort: { count: -1 } }, { $limit: 1 } ]
+    const declaracoes = await Declaracao.aggregate([
+        {
+          $match: {
+            pais_origem: "CHINA, REPUBLICA POPULAR",
+          },
+        },
+        { $group: { _id: "$mes", count: { $sum: 1 } } },
+        {
+          $project: {
+            _id: 0,
+            mes: "$_id",
+            count: "$count",
+          },
+        },
+        { $sort: { count: -1 } },
+        { $limit: 1 },
+      ]
     );
     res.status(200).json({
         status: 'success',
@@ -80,7 +99,23 @@ exports.getQuestao7 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao8 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $group: { _id: "$mes", media: { $avg: "$frete_media_dolar_truncado" } } } ]);
+    const declaracoes = await Declaracao.aggregate([
+        {
+          $group: {
+            _id: "$mes",
+            media: {
+              $avg: "$frete_media_dolar_truncado",
+            },
+          },
+        },
+        {
+          $project: {
+            _id:0,
+              mes: "$_id",
+            media: "$media"
+          }
+        }
+      ]);
     res.status(200).json({
         status: 'success',
         data: {
@@ -90,7 +125,23 @@ exports.getQuestao8 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao9 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([{ $group: { _id: "$pais_origem", media: { $avg: "$peso_liq_soma_truncado" } } }, { $sort: { media: -1 }},{$limit: 5}]
+    const declaracoes = await Declaracao.aggregate([
+        {
+          $group: {
+            _id: "$pais_origem",
+            media: { $avg: "$peso_liq_soma_truncado" },
+          },
+        },
+        {
+          $project: {
+            _id:0,
+              pais_origem: "$_id",
+            media: "$media"
+          }
+        },
+        { $sort: { media: -1 } },
+        { $limit: 5 },
+      ]
     );
     res.status(200).json({
         status: 'success',
@@ -101,7 +152,25 @@ exports.getQuestao9 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao10 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $group: { _id: "$pais_origem", media: { $avg: "$frete_media_dolar_truncado" } } }, { $sort: { media: -1 } }, { $limit: 5 } ]);
+    const declaracoes = await Declaracao.aggregate([
+        {
+          $group: {
+            _id: "$pais_origem",
+            media: {
+              $avg: "$frete_media_dolar_truncado",
+            },
+          },
+        },
+        {
+          $project: {
+            _id:0,
+              pais_origem: "$_id",
+            media: "$media"
+          }
+        },
+        { $sort: { media: -1 } },
+        { $limit: 5 },
+      ]);
     res.status(200).json({
         status: 'success',
         data: {
