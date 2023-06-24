593305576e7e8e027f090843d4187cbc7c418c8c
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-06-24 16:34:47 -0300

    Conteudo do "App.js" atualizado

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from "./components/Button";
+import Result from "./components/Result";
+import { info, question } from "./constrants";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [description, setDescription] = useState("");
 
   return (
-    <>
+    <div className='bg-cyan-100 h-full w-full flex justify-center px-4'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className='flex flex-col md:flex-row pt-14  items-center'>
+          {/* Titulo da pagina */}
+          <h1 className='text-2xl md:text-3xl font-semibold'>
+            {info[0].title}
+          </h1>
+          <div className="flex">
+          <h2 className='text-2xl md:text-3xl'> &#128230; </h2>
+          <h2 className='text-2xl md:text-3xl'> &#9992; </h2>
+          </div>
+        </div>
+        <div className='flex flex-col md:flex-row gap-12 py-8 justify-center '>
+          <div className='flex flex-col gap-3'>
+            {/* Botões referente as perguntas escolhidas*/}
+            {question.map((valor) => (
+              <Button
+                key={valor.id}
+                name={valor.name}
+                onClick={() => setDescription(valor.id)}
+              />
+            ))}
+          </div>
+          <div className="bg-white rounded-md md:w-1/2 w-full pt-2 pl-4 min-h-screen ">
+            {/* Resultado da descricao obtida a partir de qual botao foi clicado */}
+            {question
+              .filter((elemento) => elemento.id === description)
+              .map((valor) => (
+                <div key={valor.id}>
+                  {valor.description.map((description) => (
+                    <Result key={valor.id} description={description} />
+                  ))}
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="bg-black">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-red">
-        Click on the Vite and React logos to learn more
-      </p>
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
