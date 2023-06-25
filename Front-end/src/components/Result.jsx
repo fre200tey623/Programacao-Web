fcd8e38602ac948e9c8900c58053be4981cb4943
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-06-25 14:01:20 -0300

    Componente Result modificado

--- Front-end/src/components/Result.jsx
+++ Front-end/src/components/Result.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Result({paisOrigem,mes,count}){
-    return(
-        <>
-            <p>{paisOrigem}</p>
-            <p>{mes}</p>
-            <p>{count}</p>
-        </>
-    )
+export default function Result({ paisOrigem, mes, count }) {
+  return (
+    <>
+      {paisOrigem && <p className='pl-2'>{paisOrigem}</p>}
+      {mes && <p className=''>{mes}</p>}
+      {count && <p className=''>{count}</p>}
+    </>
+  );
 }
