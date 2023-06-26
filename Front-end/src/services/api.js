ebeb35d8b25624b4d93db00759f4c27164c7a388
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-06-25 23:28:44 -0300

    Rota da api atualizada

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
-export const api = axios.create({baseURL:"http://localhost:3000/api/v1/declaracao"})
+//export const api = axios.create({baseURL:"http://localhost:3000/api/v1/declaracao"})
+
+export const api = axios.create({baseURL:"http://100.27.18.226:3000/api/v1/declaracao"})
