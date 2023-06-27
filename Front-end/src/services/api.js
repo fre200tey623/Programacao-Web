482919bca004ab0e4117a6696efaebca183112ef
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-06-27 12:52:20 -0300

    rota https adicionada

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -2,4 +2,4 @@ import axios from "axios";
 
 //export const api = axios.create({baseURL:"http://localhost:3000/api/v1/declaracao"})
 
-export const api = axios.create({baseURL:"http://3.84.162.120:3000/api/v1/declaracao"})
+export const api = axios.create({baseURL:"https://52.45.166.198:3000/api/v1/declaracao"})
