f50eeae0be972e7df88614b97f3edb5ce26f7eed
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 18:17:52 -0300

    Componente Button alterado

--- Front-end/src/components/Button.jsx
+++ Front-end/src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function Button({ onClick, name }) {
+export default function Button({ onClick, name, selecionado }) {
   return (
     <button
-      className='bg-emerald-300 hover:bg-emerald-400 rounded-md md:max-w-lg px-2 py-2 font-semibold h-full min-h-[48px]'
+      className={`hover:bg-emerald-300 rounded-xl w-full py-1 border text-sm ${
+        selecionado ? "bg-emerald-300" : ""
+      } outline-none`}
       onClick={onClick}
     >
       {name}
