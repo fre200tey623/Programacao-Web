e806767e2a4f37a2b06ab643d21411f466d20695
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 18:17:16 -0300

    Componente Loading alterado

--- Front-end/src/components/Loading.jsx
+++ Front-end/src/components/Loading.jsx
@@ -1,9 +1,10 @@
-export default function Loading(){
-    return(
-        <div className="grid grid-cols-5 gap-6 animate-pulse h-12 pt-2 pl-2 border rounded-md">
-            <div className="h-2 col-span-2 bg-slate-300 rounded-full"></div>
-            <div className="h-2 bg-slate-300 rounded-full"></div>
-            <div className="h-2 bg-slate-300 rounded-full"></div>
-        </div>
-    )
+export default function Loading() {
+  return (
+    <div className='grid grid-cols-5 gap-6 animate-pulse h-12 pt-2 px-2 border rounded-md'>
+      <div className='h-2 col-span-2 bg-slate-300 rounded-full'></div>
+      <div className='h-2 bg-slate-300 rounded-full'></div>
+      <div className='h-2 bg-slate-300 rounded-full'></div>
+      <div className='h-2 bg-slate-300 rounded-full'></div>
+    </div>
+  );
 }
