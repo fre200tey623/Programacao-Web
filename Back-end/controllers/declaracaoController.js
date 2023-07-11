321a23ccca4e7bcb7b39097e1e7c3b77c1b85034
lulinha <luisfernandosouzadearaujo22@gmail.com>
2023-07-11 20:18:12 -0300

    corrigindo rotas

--- Back-end/controllers/declaracaoController.js
+++ Back-end/controllers/declaracaoController.js
@@ -2,56 +2,107 @@ const Declaracao = require('./../models/declaracaoModel');
 
 const catchAsync = require('./../utils/catchAsync');
 
+const isMesValido = (mes) => {
+  return [1,2,3,4].some(e => mes == e);
+}
 
 exports.getQuestao1 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $group: { _id: { pais_origem: "$pais_origem", mes: "$mes", }, count: { $sum: 1, }, }, }, { $project: { _id: 0, pais_origem: "$_id.pais_origem", mes: "$_id.mes", count: "$count", }, }, { $sort: { count: -1, }, }, ]);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
+  const mes = req.query.mes;
+  const sql = [
+      {
+        $group: {
+          _id: {
+            pais_origem: "$pais_origem",
+            mes: "$mes",
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          pais_origem: "$_id.pais_origem",
+          mes: "$_id.mes",
+          count: "$count",
+        },
+      },
+      {
+        $match: {
+          mes: `2023-0${mes}`
         }
-    });
+      },
+      { $sort: { count: -1 } },
+    ];
+    if (!isMesValido(mes)) {
+      sql.splice(2,1);
+    }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+        declaracoes
+      }
+  });
 });
 
 exports.getQuestao2 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate( [ { $match: { pais_origem: "ARGENTINA" } }, 
-    { $sort: { frete_maximo_dolar_truncado: -1 } }, 
-    { $project: {_id:0, pais_origem: "$pais_origem", frete_maximo_dolar_truncado:"$frete_maximo_dolar_truncado", mes: "$mes" } }, 
-    { $limit: 1 } ]
-    );
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
-    });
+  const mes = req.query.mes;
+  const sql = [ { $match: { pais_origem: "ARGENTINA" } }, 
+  { $sort: { frete_maximo_dolar_truncado: -1 } }, 
+  { $project: {_id:0, pais_origem: "$pais_origem", frete_maximo_dolar_truncado:"$frete_maximo_dolar_truncado", mes: "$mes" } },
+  {
+    $match: {
+      mes: `2023-0${mes}`
+    }
+  },
+  { $limit: 1 } ];
+  if (!isMesValido(mes)) {
+    sql.splice(3,1);
+  }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
+  });
 });
 
 exports.getQuestao3 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([
-        {
-          $group: {
-            _id: "$pais_origem",
-            count: { $sum: 1 },
-          },
-        },
-        {
-          $project: {
-            _id:0,
-            pais_origem: "$_id",
-            count: "$count",
-          },
-        },
-        { $sort: { count: -1 } },
-        { $limit: 10 },
-      ]
-    );
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
-    });
+  const mes = req.query.mes;
+  const sql = [
+    {
+      $group: {
+        _id: {pais_origem: "$pais_origem",mes: "$mes"},        
+        count: { $sum: 1 },
+      },
+    },
+    {
+      $project: {
+        _id:0,
+        pais_origem: "$_id.pais_origem",
+        mes: "$_id.mes",
+        count: "$count",
+      },
+    },
+    {
+      $match: {
+        mes: `2023-0${mes}`
+      }
+    },
+    { $sort: { count: -1 } },
+    { $limit: 10 },
+  ];
+  if (!isMesValido(mes)) {
+    sql.splice(2,1);
+  }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
+  });
 });
 
 exports.getQuestao4 = catchAsync( async(req, res, next) => {
@@ -80,88 +131,134 @@ exports.getQuestao4 = catchAsync( async(req, res, next) => {
 });
 
 exports.getQuestao5 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([
-        { $group: { _id: "$mes", count: { $sum: 1 } } },
-        {
-          $project: {
-            _id:0,
-            mes:"$_id",
-            count:"$count"
-          }
-        },
-        { $sort: { count: 1 } },
-      ]
-    );
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
-    });
+  const mes = req.query.mes;
+  const sql = [
+    { $group: { _id: "$mes", count: { $sum: 1 } } },
+    {
+      $project: {
+        _id:0,
+        mes:"$_id",
+        count:"$count"
+      }
+    },
+    {
+      $match: {
+        mes: `2023-0${mes}`
+      }
+    },
+    { $sort: { count: 1 } },
+  ];
+  if (!isMesValido(mes)) {
+    sql.splice(2,1);
+  }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
+  });
 });
 
 exports.getQuestao6 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([ { $group: { _id: "$pais_origem", media: { $avg : "$seguro_media_dolar_truncado" } } }, { $project: {_id:0, pais_origem:"$_id", valor_seguro: "$media" } }, { $sort: { valor_seguro: -1 } }, { $limit: 1 } ]
-    );
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
+  const declaracoes = await Declaracao.aggregate([
+    {
+      $group: {
+        _id: "$pais_origem",
+        media: {
+          $avg: "$seguro_media_dolar_truncado",
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        pais_origem: "$_id",
+        valor_seguro: "$media",
+      },
+    },
+    { $sort: { valor_seguro: -1 } },
+    { $limit: 1 },
+  ]
+  );
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
     });
 });
 
 exports.getQuestao7 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([
-        {
-          $match: {
-            pais_origem: "CHINA, REPUBLICA POPULAR",
-          },
-        },
-        { $group: { _id: "$mes", count: { $sum: 1 } } },
-        {
-          $project: {
-            _id: 0,
-            mes: "$_id",
-            count: "$count",
-          },
-        },
-        { $sort: { count: -1 } },
-        { $limit: 1 },
-      ]
-    );
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
-    });
+  const mes = req.query.mes;
+  const sql = [
+    {
+      $match: {
+        pais_origem: "CHINA, REPUBLICA POPULAR",
+      },
+    },
+    { $group: { _id: "$mes", count: { $sum: 1 } } },
+    {
+      $project: {
+        _id: 0,
+        mes: "$_id",
+        count: "$count",
+      },
+    },
+    {
+      $match: {
+        mes: `2023-0${mes}`
+      }
+    },
+    { $sort: { count: -1 } },
+    { $limit: 1 },
+  ];
+  if (!isMesValido(mes)) {
+    sql.splice(3,1);
+  }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
+  });
 });
 
 exports.getQuestao8 = catchAsync( async(req, res, next) => {
-    const declaracoes = await Declaracao.aggregate([
-        {
-          $group: {
-            _id: "$mes",
-            media: {
-              $avg: "$frete_media_dolar_truncado",
-            },
-          },
+  const mes = req.query.mes;
+  const sql = [
+    {
+      $group: {
+        _id: "$mes",
+        media: {
+          $avg: "$frete_media_dolar_truncado",
         },
-        {
-          $project: {
-            _id:0,
-              mes: "$_id",
-            media: "$media"
-          }
-        }
-      ]);
-    res.status(200).json({
-        status: 'success',
-        data: {
-            declaracoes
-        }
-    });
+      },
+    },
+    {
+      $project: {
+        _id:0,
+          mes: "$_id",
+        media: "$media"
+      }
+    },
+    {
+      $match: {
+        mes: `2023-0${mes}`
+      }
+    }
+  ];
+  if (!isMesValido(mes)) {
+    sql.splice(2,1);
+  }
+  const declaracoes = await Declaracao.aggregate(sql);
+  res.status(200).json({
+      status: 'success',
+      data: {
+          declaracoes
+      }
+  });
 });
 
 exports.getQuestao9 = catchAsync( async(req, res, next) => {
@@ -175,7 +272,7 @@ exports.getQuestao9 = catchAsync( async(req, res, next) => {
         {
           $project: {
             _id:0,
-              pais_origem: "$_id",
+            pais_origem: "$_id",
             media: "$media"
           }
         },
@@ -204,7 +301,7 @@ exports.getQuestao10 = catchAsync( async(req, res, next) => {
         {
           $project: {
             _id:0,
-              pais_origem: "$_id",
+            pais_origem: "$_id",
             media: "$media"
           }
         },
