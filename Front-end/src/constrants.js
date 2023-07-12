ddc90db3e8bc5eed3da4baeb8877cfa4bfd87add
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 23:09:36 -0300

    Informações das constantes atualizada

--- Front-end/src/constrants.js
+++ Front-end/src/constrants.js
@@ -7,23 +7,29 @@ export const info = [
 export const buttonsName = [
     {
         id: "1",
-        name: "Tudo"
+        name: "Tudo",
+        filtro: "",
     },
     {
         id: "2",
-        name: "Janeiro"
+        name: "Janeiro",
+        filtro: "?mes=1"
     },
     {
         id: "3",
-        name: "Fevereiro"
+        name: "Fevereiro",
+        filtro: "?mes=2"
     },
     {
         id: "4",
-        name: "Março"
+        name: "Março",
+        filtro: "?mes=3"
+
     },
     {
         id: "5",
-        name: "Abril"
+        name: "Abril",
+        filtro:"?mes=4"
     }
 
 ]
@@ -33,52 +39,62 @@ export const question = [
     {
         id:"1",
         name: "Qual a quantidade de vezes que cada país declarou importação para o Brasil?",
-        description: ["/1"]
+        description: ["/1"],
+        possuiFiltro:true
     },
     {
         id:"2",
         name: "Qual o maior valor máximo de frete dólar truncado da Argentina?",
-        description: ["/2"]
+        description: ["/2"],
+        possuiFiltro:true
     },
     {
         id:"3",
         name: "Qual os dez países que mais declaram importação para o Brasil?",
-        description: ["/3"]
+        description: ["/3"],
+        possuiFiltro:false
     },
     {
         id:"4",
         name: "Qual o número total de importações de cada país para o Brasil?",
-        description: ["/4"]
+        description: ["/4"],
+        possuiFiltro:false
     },
     {
         id:"5",
         name: "Qual a quantidade de declarações de importação de forma crescente para o Brasil?",
-        description: ["/5"]
+        description: ["/5"],
+        possuiFiltro:true
     },
     {
         id:"6",
         name: "Qual país que mais paga por importação para o Brasil?",
-        description: ["/6"]
+        description: ["/6"],
+        possuiFiltro:false
     },
     {
         id:"7",
         name: "Consultando os dados da China, dentre os meses de janeiro a abril, qual foi o mês que teve menos importações?",
-        description: ["/7"]
+        description: ["/7"],
+        possuiFiltro:true
     },
     {
         id:"8",
         name: "Qual a média dos fretes do mês de declarações de importações entre os países?",
-        description: ["/8"]
+        description: ["/8"],
+        possuiFiltro:true
     },
     {
         id:"9",
         name: "Qual foi os 5 países que tiveram as cargas mais pesadas?",
-        description: ["/9"]
+        description: ["/9"],
+        possuiFiltro:false
     },
     {
         id:"10",
         name: "Quais os 5 países em média pagam maior seguro, segundo a declaração de importação para o Brasil?",
-        description: ["/10"]
+        description: ["/10"],
+        possuiFiltro:false
     }
 
 
