9688ac27571c8f6437da18d9d62d8e5c56347d8d
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 23:13:27 -0300

    conteudo do Button alterado

--- Front-end/src/components/Button.jsx
+++ Front-end/src/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({ onClick, name, selecionado }) {
     <button
       className={`hover:bg-emerald-300 rounded-xl w-full py-1 border text-sm ${
         selecionado ? "bg-emerald-300" : ""
-      } outline-none`}
+      } outline-none border`}
       onClick={onClick}
     >
       {name}
