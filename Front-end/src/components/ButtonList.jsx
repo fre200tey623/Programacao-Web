bc9eea7ee35e80664f5b8803a9be2ed8153498ed
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 23:12:50 -0300

    conteudo do ButtonList alterado

--- Front-end/src/components/ButtonList.jsx
+++ Front-end/src/components/ButtonList.jsx
@@ -1,15 +1,21 @@
 import Button from "./Button"
 import { buttonsName } from "../constrants"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function ButtonList(){
-    const [selecionado, setSelecionado] = useState("1");
+export default function ButtonList({setValorFiltro,setSelecionado,selecionado}){
+    
+    function getFiltro(valorId,valorFiltro){
+      setSelecionado(valorId)
+      setValorFiltro(valorFiltro)
+    }
+
+
     return(
         <>
         {buttonsName.map((valor, id) => (
               <Button
                 selecionado={selecionado === valor.id}
-                onClick={() => setSelecionado(valor.id)}
+                onClick={() => getFiltro(valor.id,valor.filtro)}
                 name={valor.name}
                 key={valor.id}
               ></Button>
