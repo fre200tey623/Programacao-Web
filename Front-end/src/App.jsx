1378a549b2687f20e4fb22037e10526c1f2016f4
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-11 23:14:02 -0300

    conteudo do App alterado

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,27 +1,37 @@
 import { info, question } from "./constrants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Loading from "./components/Loading";
 import { Title } from "./components/Title";
 import { api } from "./services/api";
 import SelectBar from "./components/SelectBar";
-import ButtonList from "./components/ButtonList";
 import ResultList from "./components/ResultList";
 
 function App() {
   const [visivel, setVisivel] = useState(true);
   const [respostaData, setRespostaData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [valorFiltro, setValorFiltro] = useState("");
+  const [valorDescription,setValorDescripton] = useState("")
+  const [selecionado, setSelecionado] = useState("1");
+  const [questaoSelecionada,setQuestaoSelecionada] = useState({possuiFiltro:false})
 
-  function getDescription(description) {
-    setIsLoading(true);
+  useEffect(()=>{
+    getDescription(valorDescription);
+  },[valorFiltro,valorDescription])
+
+  function getDescription() {
+    if(valorDescription!=""){
+      setIsLoading(true);
     api
-      .get(description)
+      .get(valorDescription+valorFiltro)
       .then((response) => {
         setRespostaData(response.data.data);
       })
       .catch((error) => console.error(error.response))
       .finally(() => setIsLoading(false));
+    }
   }
+
 
   return (
     <div className='h-full w-full flex justify-center px-4 bg-stone-50'>
@@ -29,28 +39,25 @@ function App() {
         <div className='flex flex-col pt-5 md:pt-14 justify-center w-full'>
           <Title title={info[0].title} />
 
-          <div className='h-12 border  rounded-xl flex justify-center px-2 mb-2 bg-stone-50'>
+          <div className='h-12 border  rounded-xl flex justify-center px-2 mb-2 bg-stone-50 border-stone-900'>
             <SelectBar
               question={question}
-              getDescription={getDescription}
+              getDescription={setValorDescripton}
               visivel={visivel}
               setVisivel={setVisivel}
+              setQuestaoSelecionada={setQuestaoSelecionada}
             />
-          </div>
-
-          <div className='grid grid-cols-5 justify-between pt-2 pb-4 gap-1 md:gap-3'>
-            <ButtonList />
           </div>
 
           <div className='rounded-md w-full pt-2 relative min-h-screen'>
             <div className='grid gap-2'>
               {/* Resultado da descricao obtida a partir de qual botao foi clicado */}
-              {!isLoading && <ResultList respostaData={respostaData} />}
+              {!isLoading && <ResultList respostaData={respostaData} visivel={visivel} setValorFiltro={setValorFiltro} botaoVisivel={questaoSelecionada.possuiFiltro} setSelecionado={setSelecionado} selecionado={selecionado}/>}
 
               {isLoading && (
                 <div className=' flex gap-4 flex-col'>
                   {question.map((_, index) => (
-                    <Loading />
+                    <Loading key={index} />
                   ))}
                 </div>
               )}
