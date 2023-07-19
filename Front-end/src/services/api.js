3a37a0f7d0d8d54ef1cd7abb5a21eb3839e3340f
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-07-19 19:40:59 -0300

    Rota api atualizada

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+// Servidor Localhost
 //export const api = axios.create({baseURL:"https://localhost:3000/api/v1/declaracao"})
 
-export const api = axios.create({baseURL:"https://52.45.166.198:3000/api/v1/declaracao"})
+// Servidor AWS
+//export const api = axios.create({baseURL:"https://52.45.166.198:3000/api/v1/declaracao"})
+
+// Servidor Render
+export const api = axios.create({baseURL:"https://back-end-programacao-web.onrender.com/api/v1/declaracao"})
