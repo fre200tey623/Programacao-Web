aa7783dd28f4d2d42c552c1368be1567af1cd2f8
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-08-06 15:23:45 -0300

    Login atualizado

--- Front-end/src/Login.jsx
+++ Front-end/src/Login.jsx
@@ -3,20 +3,43 @@ import Button from "./components/Button";
 import Placeholder from "./components/Placeholder";
 import { rotas } from "./constrants";
 import imagemFundo from "./assets/images/5215745.jpg";
+import { api } from "./services/api";
+import { useEffect, useState } from "react";
 export default function Login() {
+  const [email,setEmail] = useState("")
+  const [senha,setSenha] = useState("")
+
+
+    function handleClick(){
+    
+      api.post("usuario/login",{
+        email: email,
+        senha: senha
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+  
+
+
   return (
     <div className='h-screen w-full flex justify-center items-center  bg-emerald-200 '>
-      <div className=' flex gap-16 bg-white rounded-lg px-12 py-6 h-3/4 drop-shadow-lg'>
+      <div className=' flex gap-16 bg-white rounded-lg px-12 py-6 h-3/4 drop-shadow-lg mx-0 md:mx-4'>
         <div className="flex flex-col h-full gap-20 pt-10">
           <div className='flex justify-center '>
             <h1 className='text-4xl'>Login</h1>
           </div>
           <div className='flex flex-col gap-3'>
             <div className='flex flex-col gap-3'>
-              <Placeholder type={"text"} placeholder={"usuario"} />
-              <Placeholder type={"password"} placeholder={"senha"} />
-              <Link to={rotas[1].sub_destino}>
-                <Button name={"Confirmar"} />
+              <Placeholder type={"text"} placeholder={"usuario"} onChange={(event)=>setEmail(event.target.value)}/>
+              <Placeholder type={"password"} placeholder={"senha"} onChange={(event)=>setSenha(event.target.value)}/>
+              <Link>
+                <Button name={"Confirmar"} onClick={()=>handleClick}/>
               </Link>
             </div>
             <p>
--- Front-end/src/components/Placeholder.jsx
+++ Front-end/src/components/Placeholder.jsx
@@ -1,7 +1,7 @@
-export default function Placeholder({placeholder,type}){
+export default function Placeholder({placeholder,type,onChange}){
     return(
         <>
-            <input type={type} placeholder={placeholder} className="border outline-none py-1 pl-2"></input>
+            <input type={type} placeholder={placeholder} className="border outline-none py-1 pl-2" onChange={onChange}></input>
         </>
     )
 }
