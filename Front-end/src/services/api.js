a71fcbcd84df7eb541551f7fa449ab0628cc08d5
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-08-06 15:23:33 -0300

    "declaracao" removida na URL da api

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -7,4 +7,4 @@ import axios from "axios";
 //export const api = axios.create({baseURL:"https://52.45.166.198:3000/api/v1/declaracao"})
 
 // Servidor Render
-export const api = axios.create({baseURL:"https://back-end-programacao-web.onrender.com/api/v1/declaracao"})
+export const api = axios.create({baseURL:"https://back-end-programacao-web.onrender.com/"})
