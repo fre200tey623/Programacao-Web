f7a79a0ad1d6d4b099f4f83ac2e57dd9a81fdfd9
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-08-11 08:42:12 -0300

    Conteudo do App atualizado

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import { rotas } from './constrants';
+import Cadastro from './Cadastro';
+import React,{useState} from 'react';
 
 const App = () => {
+
+  const [token,setToken] = useState("");
+
   return (
     <Router>
       <Routes>
-        <Route path={rotas[0].destino} element={<Login />} />
-        <Route path={rotas[1].destino} element={<Home />} />
+        <Route path={rotas[0].destino} element={<Login setToken={setToken} />} />
+        <Route path={rotas[2].destino} element={<Cadastro />} />
+        { token != ""?<Route path={rotas[1].destino} element={<Home />}/>:null}
         {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
