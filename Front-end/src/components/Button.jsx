c7e7a13cfcdcb3a90a72f89ad04bef960fa17edb
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-08-11 08:42:38 -0300

    Conteudo do Button atualizado

--- Front-end/src/components/Button.jsx
+++ Front-end/src/components/Button.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-export default function Button({ onClick, name, selecionado }) {
+export default function Button({ onClick, name, selecionado,src, className,isLoading,cor }) {
   return (
-    <button
-      className={`hover:bg-emerald-300 rounded-xl w-full py-1 border text-sm ${
+    <div>
+      <button
+      className={`${isLoading ?"flex justify-center gap-2":""} ${cor} hover:bg-emerald-400 rounded-xl w-full py-1 border text-sm ${
         selecionado ? "bg-emerald-300" : ""
       } outline-none border`}
       onClick={onClick}
     >
       {name}
+      {isLoading &&
+      <img src={src} className={className}></img>}
     </button>
+    </div>
   );
 }
