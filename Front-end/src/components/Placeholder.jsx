8779ae53f2d3b1a190196f1176ae534e3be6734d
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-08-14 10:23:45 -0300

    Componente Placeholder atualizado

--- Front-end/src/components/Placeholder.jsx
+++ Front-end/src/components/Placeholder.jsx
@@ -1,7 +1,7 @@
 export default function Placeholder({placeholder,type,onChange}){
     return(
         <>
-            <input type={type} placeholder={placeholder} className="border outline-none py-1 pl-2" onChange={onChange}></input>
+            <input type={type} placeholder={placeholder} className="border border-gray-400 outline-none py-1 pl-2 rounded-xl" onChange={onChange}></input>
         </>
     )
 }
