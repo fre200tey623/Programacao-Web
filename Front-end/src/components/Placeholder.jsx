d088788e381a0245686600e0d6e1a94350a21c92
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2023-09-19 20:25:44 -0300

    Texto modo escuro corrigido

--- Front-end/src/components/Placeholder.jsx
+++ Front-end/src/components/Placeholder.jsx
@@ -1,7 +1,7 @@
 export default function Placeholder({placeholder,type,onChange,className}){
     return(
         <>
-            <input type={type} placeholder={placeholder} className={`border border-gray-400 outline-none py-1 pl-2 rounded-xl ${className}`} onChange={onChange}></input>
+            <input type={type} placeholder={placeholder} className={`border border-gray-400 outline-none py-1 pl-2 rounded-xl ${className} text-slate-950`} onChange={onChange}></input>
         </>
     )
 }
